a3dddef9c0aba771c6c968fd78b035a75d758845
xtrm <oss@xtrm.me>
2022-12-14 21:45:25 +0100

    better everything uwu

---
+++ src/main/java/dev/mayaqq/chattoggle/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,35 @@
+package dev.mayaqq.chattoggle.mixin;
+
+import dev.mayaqq.chattoggle.Config;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayerEntity.class)
+public abstract class ClientPlayerEntityMixin {
+    @Shadow
+    public abstract void sendCommand(String command, @Nullable Text preview);
+
+    @Inject(
+            method = "sendChatMessage",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void onSendMessage(String message, Text preview, CallbackInfo ci) {
+        if (Config.INSTANCE.on) {
+            if (!message.startsWith(Config.INSTANCE.messagePrefix)) {
+                // call with modified message
+                String modified = Config.INSTANCE.messagePrefix + message;
+                sendCommand(modified, preview);
+
+                // cancel original method
+                ci.cancel();
+            }
+        }
+    }
+}
